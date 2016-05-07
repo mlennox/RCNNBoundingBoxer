b9947778a43f286f95fcee2a8f28149b64c91cfc
mlennox <mark@webpusher.ie>
2016-05-08 00:27:00 +0100

    adding circle-ci config and working on the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,40 @@
-module.exports = {
-    entry: './app/app.js',
-    output: {
-        path: __dirname + '/build/ui/',
-        filename: 'bundle.js'
+import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import { createLogger } from './logger/'
+
+var logger = createLogger('webpack','config')
+
+const webpack_config = {
+    name: 'RCNN',
+    target: 'web',
+    resolve: {
+        root: __dirname,
+        extensions: ['', '.js', '.jsx']
     },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                include: [],
-                exclude: [/node_modules/],
-                loader: 'babel',
-                query: {presets: ['es2015', 'stage-3', 'react']}
-            }
-        ]
-    }
+    module: {}
 }
+
+webpack_config.entry = {
+    app: __dirname + '/app/app.js'
+    // , vendor: []
+}
+
+webpack_config.output = {
+    path: __dirname + '/build/ui/',
+    filename: 'bundle.js'
+}
+
+webpack_config.module.loaders = [
+    {
+        test: /\.(js|jsx)$/,
+        exclude: [/node_modules/],
+        loader: 'babel',
+        query: {
+            presets: ['es2015', 'stage-3', 'react']
+        }
+    },
+    {
+        test: /\.json$/,
+        loader: 'json'
+    }
+]
